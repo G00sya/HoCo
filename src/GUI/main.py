91199bec93c03a3ca73546e515ec537f0e9021f5
ShatunovEvgeniy <shatunov868@gmail.com>
2024-04-10 21:13:40 +0300

    feat: Set up body

--- src/GUI/main.py
+++ src/GUI/main.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import *
-from PyQt5.Qt import *
 from PyQt5.Qsci import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 import sys
 import os
@@ -10,11 +10,15 @@ from pathlib import Path
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.window_font = None
+        super(QMainWindow, self).__init__()
+        self.side_bar_clr = "#282c34"
         self.init_ui()
 
+        self.window_font = None
         self.current_file = None
+
+        self.sidebar_frame = None
+        self.tree_frame = None
 
     def init_ui(self):
         self.setWindowTitle("HoCo code editor")
@@ -24,7 +28,7 @@ class MainWindow(QMainWindow):
         stylesheet_path = os.path.join(dir_path, "../../static/css/style.qss")
         self.setStyleSheet(open(stylesheet_path, "r").read())
 
-        self.window_font = QFont("Fire Code")
+        self.window_font = QFont("Fire Code")  # must be installed
         self.window_font.setPointSize(16)
         self.setFont(self.window_font)
 
@@ -39,7 +43,7 @@ class MainWindow(QMainWindow):
 
         :return:
         """
-
+# Menu -----------------------------------------------------------------------------------------------------------------
     def set_up_menu(self):
         """
 
@@ -71,8 +75,62 @@ class MainWindow(QMainWindow):
     def open_folder(self):
         pass
 
+# Body -----------------------------------------------------------------------------------------------------------------
     def set_up_body(self):
-        pass
+        """
+
+        :return:
+        """
+        body_frame = QFrame()
+        body_frame.setFrameShape(QFrame.NoFrame)
+        body_frame.setFrameShadow(QFrame.Plain)
+        body_frame.setLineWidth(0)
+        body_frame.setMidLineWidth(0)
+        body_frame.setContentsMargins(0, 0, 0, 0)
+        body_frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        body = QHBoxLayout()
+        body.setContentsMargins(0, 0, 0, 0)
+        body.setSpacing(0)
+        body_frame.setLayout(body)
+
+        # Sidebar frame ------------------------------------------------------------------------------------------------------
+        self.sidebar_frame = QFrame()
+        self.sidebar_frame.setFrameShape(QFrame.StyledPanel)
+        self.sidebar_frame.setFrameShadow(QFrame.Plain)
+        self.sidebar_frame.setStyleSheet(f'''
+                    background-color: {self.side_bar_clr};
+                ''')
+        sidebar_frame_layout = QHBoxLayout()
+        sidebar_frame_layout.setContentsMargins(5, 10, 5, 0)
+        sidebar_frame_layout.setSpacing(0)
+        sidebar_frame_layout.setAlignment(Qt.AlignTop | Qt.AlignCenter)
+        self.sidebar_frame.setLayout(sidebar_frame_layout)
+
+        body.addWidget(self.sidebar_frame)
+        self.hsplit = QSplitter(Qt.Horizontal)
+
+        # File manager frame -------------------------------------------------------------------------------------------
+        self.tree_frame = QFrame()
+        self.tree_frame.setLineWidth(1)
+        self.tree_frame.setMaximumWidth(400)
+        self.tree_frame.setMinimumWidth(200)
+        self.tree_frame.setBaseSize(100, 0)
+        self.tree_frame.setContentsMargins(0, 0, 0, 0)
+        tree_frame_layout = QVBoxLayout()
+        tree_frame_layout.setContentsMargins(0, 0, 0, 0)
+        tree_frame_layout.setSpacing(0)
+        self.tree_frame.setStyleSheet('''
+            QFrame {
+                background-color: #21252b;
+                border-radius: 5px;
+                border: none;
+                padding: 5px;
+                color: #D3D3D3;
+            }
+            QFrame: hover {
+                color: white;
+            }
+        ''')
 
 
 if __name__ == "__main__":
