221daab0ac234e6acdee1d1fea7eda6a0bf55022
byllet <1rasim1@list.ru>
2024-05-15 14:54:24 +0300

    Added mid test

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -17,7 +17,7 @@ def TreesEqual(lhs : Node, rhs: Node):
         lhs_child = lhs.childs[i]
         rhs_child = rhs.childs[i]
         if not TreesEqual(lhs_child, rhs_child):
-            return false
+            return False
 
     return True
 
@@ -43,5 +43,69 @@ def test_SmallTree():
 
     prog = Node('VeKrestKrestProg')
     prog.AddChild(small_tree)
+
+    assert TreesEqual(ast, prog)
+
+    
+def test_MidFunc():
+    small = open('mid_func.txt', 'r')
+    code = small.read()
+    scanner = Scanner(code)
+    parser = Parser()
+    ast = parser.Parse(scanner).GetRoot()
+
+    mid_tree = Node(value='main')
+
+    a_p = Node('a')
+    a_p.AddChild(Node('type: celina'))
+
+    b_p = Node('b')
+    b_p.AddChild(Node('type: bukvi'))
+
+    params = Node('params')
+    params.AddChild(a_p)
+    params.AddChild(b_p)
+    
+
+    ret = Node('return type: bukvi')
+
+    body = Node('body')
+
+    comp = Node('>')
+    comp.AddChild(Node('a'))
+    comp.AddChild(Node('10'))
+
+    cond = Node('condition')
+    cond.AddChild(comp)
+
+    koli = Node('KOLI')
+
+    eq = Node('=')
+    eq.AddChild(Node('b'))
+    eq.AddChild(Node('"123"'))
+
+    stat_koli = Node('statements')
+    stat_koli.AddChild(eq)
+
+    koli.AddChild(cond)
+    koli.AddChild(stat_koli)
+
+    plus = Node('+')
+    plus.AddChild(Node('b'))
+    plus.AddChild(Node('"321"'))
+    vozd = Node('VOZDAT')
+    vozd.AddChild(plus)
+
+    body.AddChild(koli)
+    body.AddChild(vozd)
+
+    mid_tree.AddChild(params)
+    mid_tree.AddChild(ret)
+    mid_tree.AddChild(body)
+
+    prog = Node('VeKrestKrestProg')
+    prog.AddChild(mid_tree)
+
+    mid_tree.Print()
 
     assert TreesEqual(ast, prog)
